2efa03384abffac97d38593fae7f3f1927f6f3ae
Vito Charinas <v.charinas@webapp-solutions.co.uk>
2019-06-23 17:48:33 +0100

    a lot

--- site/web/themes/custom/edu/pagepiling/initiate.js
+++ site/web/themes/custom/edu/pagepiling/initiate.js
@@ -6,7 +6,16 @@
 
             direction: 'vertical',
 
+            onLeave: function(index, nextIndex, direction){
+			//after leaving section 2
+			if(index == 1 && direction =='down'){
+				//alert("Going to section 2!");
+			}
 
+			else if(index == 2 && direction == 'up'){
+				//alert("Going to section 1!");
+			}
+		}
             });
 
     });
