78320b6b770732a8eb440a42ad20e756a6220391
Vito Charinas <v.charinas@webapp-solutions.co.uk>
2019-06-24 19:05:08 +0100

    styling edtions

---
+++ site/web/sites/default/files/php/twig/5d11103c0a301___string_template__dac327_ooLpzbceBaYlKXMvVefopzbUH/hXG_HfCbowmCiSuj6j5YIFyqy8ziTByItMqK_07U3ng.php
@@ -0,0 +1,106 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__dac3275b0ff5ebbcffac4e3f9fe9061c0b26fc35d4dd8ca16cf7e78663419abd */
+class __TwigTemplate_43b6d18fba38c373be7d0218295f1e6147b01ed1473a67fc1ec8fe0cf84c7c78 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 1];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453 = $this->env->getExtension("Drupal\\webprofiler\\Twig\\Extension\\ProfilerExtension");
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->enter($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "__string_template__dac3275b0ff5ebbcffac4e3f9fe9061c0b26fc35d4dd8ca16cf7e78663419abd"));
+
+        // line 1
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_section_image_"] ?? null)), "html", null, true);
+        echo " 
+<h3>";
+        // line 2
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_h3_tag"] ?? null)), "html", null, true);
+        echo "</h3>
+<h2>";
+        // line 3
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+        echo "</h2>
+";
+        // line 4
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_text"] ?? null)), "html", null, true);
+        
+        $__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453->leave($__internal_b8a44bb7188f10fa054f3681425c559c29de95cd0490f5c67a67412aafc0f453_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__dac3275b0ff5ebbcffac4e3f9fe9061c0b26fc35d4dd8ca16cf7e78663419abd";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  70 => 4,  66 => 3,  62 => 2,  58 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{# inline_template_start #}{{ field_section_image_ }} 
+<h3>{{ field_h3_tag }}</h3>
+<h2>{{ title }}</h2>
+{{ field_text }}", "__string_template__dac3275b0ff5ebbcffac4e3f9fe9061c0b26fc35d4dd8ca16cf7e78663419abd", "");
+    }
+}
--- site/web/themes/custom/edu/pagepiling/initiate.js
+++ site/web/themes/custom/edu/pagepiling/initiate.js
@@ -9,11 +9,16 @@
             onLeave: function(index, nextIndex, direction){
 			//after leaving section 2
 			if(index == 1 && direction =='down'){
-				//alert("Going to section 2!");
+                //alert("Going to section 2!");
+                $('header').css({'display':'block'});
+                $('footer').css({'color':'black'});
+
 			}
 
 			else if(index == 2 && direction == 'up'){
-				//alert("Going to section 1!");
+                //alert("Going to section 1!");
+                $('header').css({'display':'none'});
+                $('footer').css({'color':'white'});
 			}
 		}
             });
