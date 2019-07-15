11654a661b5b499568e4fe8d4f54294edfebfb37
Vito Charinas <v.charinas@webapp-solutions.co.uk>
2019-07-15 10:07:26 +0100

    split logged user from visitor on front page

--- site/web/themes/custom/edu/pagepiling/initiate.js
+++ site/web/themes/custom/edu/pagepiling/initiate.js
@@ -1,28 +1,25 @@
-
-(function ($) {
+(function($) {
     // All your code here
     $(document).ready(function() {
         $('#pagepiling').pagepiling({
 
             direction: 'vertical',
 
-            onLeave: function(index, nextIndex, direction){
-			//after leaving section 2
-			if(index == 1 && direction =='down'){
-                //alert("Going to section 2!");
-                $('header').css({'display':'inline-grid'});
+            onLeave: function(index, nextIndex, direction) {
+                //after leaving section 2
+                if (index == 1 && direction == 'down') {
+                    //alert("Going to section 2!");
+                    $('header').css({ 'display': 'flex' });
 
-			}
+                } else if (index == 2 && direction == 'up') {
+                    //alert("Going to section 1!");
+                    $('header').css({ 'display': 'none' });
 
-			else if(index == 2 && direction == 'up'){
-                //alert("Going to section 1!");
-                $('header').css({'display':'none'});
-
-			}
-		}
-            });
+                }
+            }
+        });
 
     });
 
 
-  }(jQuery));
+}(jQuery));
